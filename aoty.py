6254f4d7a15eef9b93cf742620e202e6383de83a
Marin Boban <79663595+ma31n@users.noreply.github.com>
2026-02-18 17:12:41 +0100

    TESTING - Update CORS configuration to allow all origins
    Removed specific CORS origin restriction for the app.

--- aoty.py
+++ aoty.py
@@ -3,13 +3,14 @@ from flask_cors import CORS
 from albumoftheyearapi import AOTY
 
 app = Flask(__name__)
-CORS(app, origins=["https://ma31n.github.io"])
+CORS(app)
 
 @app.route("/user/<username>")
 def get_user(username):
     client = AOTY()
     info = client.user_ratings_all(username, max_pages=None)
     return jsonify(info)
+
 
 
 
