8cecb9ab4535e6246ded188bfd6606a41081de86
Marin Boban <79663595+ma31n@users.noreply.github.com>
2026-02-18 17:06:19 +0100

    Add CORS support to Flask application

--- aoty.py
+++ aoty.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from albumoftheyearapi import AOTY
 
 app = Flask(__name__)
+CORS(app, origins=["https://ma31n.github.io"])
 
 @app.route("/user/<username>")
 def get_user(username):
     client = AOTY()
     info = client.user_ratings_all(username, max_pages=None)
     return jsonify(info)
+
 
 
